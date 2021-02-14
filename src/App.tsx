a7894c870f0a3730bb08079600cfdfdd5e02d3a0
Romulo Lima <49794825+rosll@users.noreply.github.com>
2021-02-14 00:26:57 -0300

    parte visual/validação no form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Signin from './pages/signin'
+//import Signup from './pages/signup'
+import GlobalStyle from './styles/Global'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <Signin />
+    <GlobalStyle />
+  </>
+)
 
 export default App;
